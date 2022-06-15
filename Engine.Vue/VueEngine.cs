03c5b218b172fceea7d48e56ac6c734bf2a2b9f7
dmassana <dmassana@sannicolas.com.sv>
2022-06-14 21:30:45 -0600

    Adding vue engine project

---
+++ Engine.Vue/VueEngine.cs
@@ -0,0 +1,26 @@
+﻿using CrossBind.Engine;
+using CrossBind.Engine.BaseModels;
+using System.Text;
+
+namespace Engine.Vue
+{
+    public class VueEngine : IEngine
+    {
+        public string PluginName => "Vue Engine Official";
+
+        public int MajorVersion => 0;
+
+        public int MinorVersion => 1;
+
+        public int PathVersion => 0;
+
+        public EngineTarget Target => EngineTarget.vue;
+
+        public string CompileUnit(UnitModel unit, bool production)
+        {
+            var stringBuilder = new StringBuilder();
+
+            return stringBuilder.ToString();
+        }
+    }
+}
