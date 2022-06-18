8d8f838d9e7adccc823c2f7172e01a35a1f989dc
Yury Rivera <shulkmaster@gmail.com>
2022-06-17 21:36:38 -0600

    #15 adding support for z-index and wodth visitor

---
+++ CrossBind.Compiler.Test/StylesTest/BasicStylesTest.cs
@@ -0,0 +1,45 @@
+﻿using Antlr4.Runtime;
+using CrossBind.Compiler.Visitors.Component;
+using CrossBind.Engine.StyleModel;
+
+namespace CrossBind.Compiler.Test.StylesTest;
+
+public class BasicStylesTest
+{
+
+    private static HaibtParser BuildParser(string code)
+    {
+        var charStream = new AntlrInputStream(code);
+        var lexer = new HaibtLexer(charStream);
+        var stream = new CommonTokenStream(lexer);
+        return new HaibtParser(stream);
+    }
+    
+    [Theory]
+    [InlineData("zIndex : 5;")]
+    [InlineData("zIndex:5;")]
+    [InlineData("zIndex:5;\n\r")]
+    [InlineData("zIndex:   5;\n\r")]
+    [InlineData(" zIndex:5;\n\r")]
+    public void Should_Parse_Z_Index(string code)
+    {
+        HaibtParser parser = BuildParser(code);
+        var visitor = new StyleVisitor();
+        ComponentStyle result = visitor.Visit(parser.css_statement());
+        Assert.Equal("z-index: 5;\n", result.StringValue);
+    }
+    
+    [Theory]
+    [InlineData("width : 0;", "px",0f)]
+    [InlineData("width: 250px;", "px", 250f)]
+    [InlineData("width:30em;\n\r", "em", 30f)]
+    [InlineData("width:   5rem;\n\r", "rem", 5f)]
+    [InlineData(" width:5.75em;\n\r", "em", 5.75f)]
+    public void Should_Parse_Width(string code, string unit, float wVal)
+    {
+        HaibtParser parser = BuildParser(code);
+        var visitor = new StyleVisitor();
+        ComponentStyle result = visitor.Visit(parser.css_statement());
+        Assert.Equal($"width: {wVal}{unit};\n", result.StringValue);
+    }
+}
--- CrossBind.Compiler/Visitors/Component/StyleVisitor.cs
+++ CrossBind.Compiler/Visitors/Component/StyleVisitor.cs
@@ -4,6 +4,7 @@ namespace CrossBind.Compiler.Visitors.Component;
 
 public class StyleVisitor : HaibtBaseVisitor<ComponentStyle>
 {
+
     public override ComponentStyle VisitBgColor(HaibtParser.BgColorContext context)
     {
         var visitBgColor = new ComponentStyle
@@ -14,34 +15,69 @@ public class StyleVisitor : HaibtBaseVisitor<ComponentStyle>
         return visitBgColor;
     }
 
+    private static BorderRule VisitBorderRule(HaibtParser.BorderValueContext context)
+    {
+        var stroke = context.cssMeasure();
+        string? strokeWidth = stroke.NUMBER()?.GetText();
+        int width = 1;
+        if (!string.IsNullOrWhiteSpace(strokeWidth))
+        {
+            width = int.Parse(strokeWidth);
+        }
+        
+        return new BorderRule
+        {
+            Color = context.HEX_COLOR()?.GetText() ?? string.Empty,
+            Stroke = width,
+            Unit = stroke.CSS_UNIT()?.GetText() ?? "px",
+            BorderType = context.BORDER_STYLE().GetText(),
+        };
+    }
+
     public override ComponentStyle VisitInlineBorder(HaibtParser.InlineBorderContext context)
     {
-        var border = new ComponentStyle
-        {
-            Key = StyleBorder.BorderKey,
-            StringValue = $"{context.NUMBER().GetText()}px {context.IDENTIFIER().GetText()} {context.HEX_COLOR()}",
-        };
+        BorderRule value = VisitBorderRule(context.borderValue());
+        var border = new StyleBorder(value);
         return border;
     }
 
     public override ComponentStyle VisitCompoundBorder(HaibtParser.CompoundBorderContext context)
     {
-        int count = context.ChildCount;
-        var nodes = new string[count - 4];
-        if (nodes.Length > 0)
+        var borderValues = context.borderValue();
+        var borders = new BorderRule[4];
+        for (int index = 0; index < borderValues.Length; index++)
         {
-            for (var i = 0; i < nodes.Length; i++)
-            {
-                nodes[i] = context.GetChild(i + 3).GetText();
-            }
+            var borderValue = borderValues[index];
+            borders[index] = VisitBorderRule(borderValue);
         }
-        string? style = nodes.Reduce((prev, current) => prev+ " " + current);
 
-        var border = new StyleBorder
-        {
-            Key = StyleBorder.BorderKey,
-            StringValue = style,
-        };
+        var border = new StyleBorder(borders);
         return border;
+    }
+
+    public override ComponentStyle VisitZIndex(HaibtParser.ZIndexContext context)
+    {
+        return new ComponentStyle
+        {
+            Key = "z-index",
+            StringValue = $"z-index: {context.NUMBER().GetText()};\n",
+        };
+    }
+
+    private static string FromMeasure(HaibtParser.CssMeasureContext ctx)
+    {
+        string unit = ctx.CSS_UNIT()?.GetText() ?? "px";
+        return ctx.NUMBER().GetText() + unit;
+    }
+
+    public override ComponentStyle VisitWidth(HaibtParser.WidthContext context)
+    {
+        string key = context.Width().GetText();
+        string measure = FromMeasure(context.cssMeasure());
+        return new ComponentStyle
+        {
+            Key = key,
+            StringValue = $"{key}: {measure};\n",
+        };
     }
 }
