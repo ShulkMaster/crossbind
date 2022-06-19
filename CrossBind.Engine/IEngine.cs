5b9b8d130b5d731f733958badf7b9c379a39bfe1
Yury Rivera <shulkmaster@gmail.com>
2022-06-18 21:50:26 -0600

    :memo: agregando binarios para CI y documentacion

--- CrossBind.Engine/IEngine.cs
+++ CrossBind.Engine/IEngine.cs
@@ -3,15 +3,18 @@ using CrossBind.Engine.Generated;
 
 namespace CrossBind.Engine;
 
+/// <include file='Docs/Api/IEngine.xml' path='docs/class/*'/>
 public interface IEngine
 {
+    /// <include file='Docs/Api/IEngine.xml' path='docs/PluginId/*'/>
     public string PluginId { get; }
     public string PluginName { get; }
     public int MajorVersion { get; }
     public int MinorVersion { get; }
     public int PathVersion { get; }
     public EngineTarget Target { get; }
-
+    
+    /// <include file='Docs/Api/IEngine.xml' path='docs/CompileUnitFile/*'/>
     public SourceFile[] CompileUnit(UnitModel unit, bool production);
 
 }
