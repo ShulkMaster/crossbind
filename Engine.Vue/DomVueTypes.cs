d9fdaece54f57d78adbe0e2e0a48a7adae242dd5
David Massana <david_massana@hotmail.com>
2022-06-19 21:37:48 -0600

    Changes in vue engine

---
+++ Engine.Vue/DomVueTypes.cs
@@ -0,0 +1,20 @@
+﻿using CrossBind.Engine.ComponentModels;
+
+namespace Engine.Vue;
+
+public static class DomVueTypes
+{
+    public static string GetComponentTag(Extendable extend)
+    {
+        return extend switch
+        {
+            Extendable.Button => "button",
+            Extendable.Select => "select",
+            Extendable.TextBox => "input",
+            Extendable.Component => string.Empty,
+            _ => throw new ArgumentOutOfRangeException(nameof(extend), extend, null)
+        };
+    }
+}
+
+
--- Engine.Vue/VueEngine.cs
+++ Engine.Vue/VueEngine.cs
@@ -1,11 +1,15 @@
 ﻿using CrossBind.Engine;
 using CrossBind.Engine.BaseModels;
+using CrossBind.Engine.ComponentModels;
+using CrossBind.Engine.Generated;
 using System.Text;
 
 namespace Engine.Vue
 {
     public class VueEngine : IEngine
     {
+        public string PluginId => "Vue.Official";
+
         public string PluginName => "Vue Engine Official";
 
         public int MajorVersion => 0;
@@ -14,13 +18,54 @@ namespace Engine.Vue
 
         public int PathVersion => 0;
 
-        public EngineTarget Target => EngineTarget.vue;
+        public EngineTarget Target => EngineTarget.Vue;
 
-        public string CompileUnit(UnitModel unit, bool production)
+        private static SourceFile CompileComponent(ComponentModel model, StringBuilder sb)
         {
-            var stringBuilder = new StringBuilder();
+            string TemplateTag = "template";
+            string ScriptTag = "script";
+            string StyleTag = "style";
 
-            return stringBuilder.ToString();
+            sb.Append($"<{TemplateTag}>\n");
+
+            string tagIncome = DomVueTypes.GetComponentTag(model.Extends);
+
+            sb.Append($"\t<{tagIncome} props=\"\">\n");
+            sb.Append($"</{TemplateTag}>");
+
+            return new SourceFile(model.Name, "vue");
+
+        }
+
+        private static SourceFile CompileModel(BindModel model, StringBuilder sb)
+        {
+            switch (model)
+            {
+                case ComponentModel compModel:
+                    {
+                        return CompileComponent(compModel, sb);
+                    }
+            }
+
+            return new SourceFile("", "");
+        }
+
+        public SourceFile[] CompileUnit(UnitModel unit, bool production)
+        {
+            var sb = new StringBuilder();
+            string baseName = Path.GetFileName(unit.FilePath);
+            int dotIndex = baseName.IndexOf('.');
+            string fileName = baseName[..dotIndex];
+
+            var files = new List<SourceFile>();
+
+            foreach (var model in unit.Models)
+            {
+                var source = CompileModel(model, sb);
+                files.Add(source);
+            }
+
+            return files.ToArray();
         }
     }
 }
