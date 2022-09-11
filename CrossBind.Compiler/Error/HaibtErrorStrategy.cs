bccea67a5fd9ba5a670bb1bb95cc3dfd9799b190
Yury Rivera <shulkmaster@gmail.com>
2022-09-11 16:01:14 -0600

    actualizando creacion clases

--- CrossBind.Compiler/Error/HaibtErrorStrategy.cs
+++ CrossBind.Compiler/Error/HaibtErrorStrategy.cs
@@ -4,7 +4,7 @@ namespace CrossBind.Compiler.Error;
 
 public class HaibtErrorStrategy: DefaultErrorStrategy
 {
-    public override void ReportError(Parser recognizer, RecognitionException e)
+    public override void ReportError(Antlr4.Runtime.Parser recognizer, RecognitionException e)
     {
         Console.WriteLine(recognizer.CurrentToken.Line);
         Console.WriteLine(e);
