707a66575e97c990ab6f999ba0839db0b11984ee
Yury Rivera <shulkmaster@gmail.com>
2022-09-15 17:47:39 -0600

    :rocket: updating powershell setup.ps1

--- Gen/setup.ps1
+++ Gen/setup.ps1
@@ -2,6 +2,15 @@
 $pack = "CrossBind.Compiler.Parser"
 $ourDir = ".\CrossBind.Compiler\Parser\"
 
+if (Test-Path $ourDir) {
+    Write-Host "Folder Exists"
+}
+else
+{
+    New-Item $ourDir -ItemType Directory
+    Write-Host "Folder Created successfully"
+}
+
 Remove-Item .\CrossBind.Compiler\Parser\* -Recurse -Force
 
 java -jar Gen\antlr.jar -lib $libDir -Dlanguage=CSharp -encoding UTF8 -Xexact-output-dir -package $pack -o $libDir .\CrossBind.Compiler\Lang\HaibtLexer.g4
